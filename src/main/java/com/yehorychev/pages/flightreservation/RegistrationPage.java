658bb987a74d46100e0d2972499f3cebf331a594
YehorYehorychev <egor57899@gmail.com>
2024-06-06 18:24:54 -0700

    All WebElements from Registration page added to RegistrationPage class

--- src/main/java/com/yehorychev/pages/flightreservation/RegistrationPage.java
+++ src/main/java/com/yehorychev/pages/flightreservation/RegistrationPage.java
@@ -1,26 +1,30 @@
 package com.yehorychev.pages.flightreservation;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage {
+    private final WebDriver driver;
+
     @FindBy(id = "firstName")
-    private WebElement firstname;
+    private WebElement firstNameInput;
 
     @FindBy(id = "lastName")
-    private WebElement lastname;
+    private WebElement lastNameInput;
 
     @FindBy(id = "email")
-    private WebElement email;
+    private WebElement emailInput;
 
     @FindBy(id = "password")
-    private WebElement password;
+    private WebElement passwordInput;
 
     @FindBy(css = "[placeholder='1234 Main St']")
-    private WebElement street;
+    private WebElement streetInput;
 
     @FindBy(css = "input[name='city']")
-    private WebElement city;
+    private WebElement cityInput;
 
     @FindBy(css = "#inputState")
     private WebElement stateDropDown;
@@ -29,5 +33,37 @@ public class RegistrationPage {
     private WebElement state;
 
     @FindBy(css = "input[name='zip']")
-    private WebElement zip;
+    private WebElement zipInput;
+
+    @FindBy(id = "register-btn")
+    private WebElement registerButton;
+
+    public RegistrationPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goTo(String url) {
+        this.driver.get(url);
+    }
+
+    public void enterUserDetails(String firstName, String lastName) {
+        this.firstNameInput.sendKeys(firstName);
+        this.lastNameInput.sendKeys(lastName);
+    }
+
+    public void enterUserCredentials(String email, String password) {
+        this.emailInput.sendKeys(email);
+        this.passwordInput.sendKeys(password);
+    }
+
+    public void enterAddress(String street, String city, String zip) {
+        this.streetInput.sendKeys(street);
+        this.cityInput.sendKeys(city);
+        this.zipInput.sendKeys(zip);
+    }
+
+    public void register() {
+        this.registerButton.click();
+    }
 }
