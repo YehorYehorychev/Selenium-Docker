3e1932d224b4d7e36cf743cab5099ac6f75d9d6b
YehorYehorychev <egor57899@gmail.com>
2024-06-11 17:36:11 -0700

    BeforeTest driver has been added to the FlightReservationTest class

--- src/test/java/com/yehorychev/tests/flightreservation/FlightReservationTest.java
+++ src/test/java/com/yehorychev/tests/flightreservation/FlightReservationTest.java
@@ -1,13 +1,22 @@
 package com.yehorychev.tests.flightreservation;
 
 import com.yehorychev.pages.flightreservation.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FlightReservationTest {
 
     private WebDriver driver;
+
+    @BeforeTest
+    public void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+    }
 
     @Test
     public void userRegistrationTest() {
@@ -51,7 +60,6 @@ public class FlightReservationTest {
     public void flightReservationConfirmationTest() {
         FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
         Assert.assertTrue(flightConfirmationPage.isAt());
-
         Assert.assertEquals(flightConfirmationPage.getPrice(), "$1169 USD");
     }
 }
