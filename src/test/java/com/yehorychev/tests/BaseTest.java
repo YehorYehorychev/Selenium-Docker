7d8ac869b03634ada69cab36d23031feb44809e6
YehorYehorychev <egor57899@gmail.com>
2024-06-17 09:43:04 -0700

    getRemoteDriver() method added to BaseTest class

--- src/test/java/com/yehorychev/tests/BaseTest.java
+++ src/test/java/com/yehorychev/tests/BaseTest.java
@@ -1,10 +1,17 @@
 package com.yehorychev.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
     protected WebDriver driver;
@@ -13,6 +20,16 @@ public abstract class BaseTest {
     public void setDriver() {
         WebDriverManager.chromedriver().setup();
         this.driver = new ChromeDriver();
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
     }
 
     private WebDriver getLocalDriver() {
