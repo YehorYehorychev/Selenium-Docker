1c7ae284466ce1059290c712b6420549d17df215
YehorYehorychev <egor57899@gmail.com>
2024-06-20 16:08:32 -0700

    Config class created in util package

---
+++ src/test/java/com/yehorychev/tests/util/Config.java
@@ -0,0 +1,10 @@
+package com.yehorychev.tests.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Config {
+    private static final Logger log = LoggerFactory.getLogger(Config.class);
+    private static final String DEFAULT_PROPERTIES = "config/default.properties";
+
+}
