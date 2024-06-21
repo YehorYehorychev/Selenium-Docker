101009e05d9130cb55c9476b6ff1c2cb682ce60f
YehorYehorychev <egor57899@gmail.com>
2024-06-20 17:05:28 -0700

    getRemoteDriver method has been refactored usuing constants in BaseTest class

--- src/test/java/com/yehorychev/tests/BaseTest.java
+++ src/test/java/com/yehorychev/tests/BaseTest.java
@@ -1,5 +1,7 @@
 package com.yehorychev.tests;
 
+import com.yehorychev.tests.util.Config;
+import com.yehorychev.tests.util.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -16,6 +20,7 @@ import java.net.URL;
 
 public abstract class BaseTest {
     protected WebDriver driver;
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
 
     @BeforeTest
     public void setDriver() throws MalformedURLException {
@@ -37,13 +42,15 @@ public abstract class BaseTest {
     }*/
 
     private WebDriver getRemoteDriver() throws MalformedURLException {
-        Capabilities capabilities;
-        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
-            capabilities = new ChromeOptions();
-        } else {
+        Capabilities capabilities = new ChromeOptions();
+        if (Constants.FIREFOX.equalsIgnoreCase(Config.get(Constants.BROWSER))) {
             capabilities = new FirefoxOptions();
         }
-        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        String urlFormat = Config.get(Constants.GRID_URL_FORMAT);
+        String hubHost = Config.get(Constants.GRID_HUB_HOST);
+        String url = String.format(urlFormat, hubHost);
+        log.info("Grid URL: {}", url);
+        return new RemoteWebDriver(new URL(url), capabilities);
     }
 
 /*    private WebDriver getRemoteDriver(String browser) throws MalformedURLException {
